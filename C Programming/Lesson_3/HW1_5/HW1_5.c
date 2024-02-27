5050773298859f1c0118b62bf8d5debd9072f12f
Muhammadatta158 <148547651+Muhammadatta158@users.noreply.github.com>
2024-02-27 22:20:52 +0200

    Add files via upload

---
+++ C Programming/Lesson_3/HW1_5/HW1_5.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main ()
+{
+    char i;
+    printf("Please enter the character you need to know its ASCII code ");
+    i=getchar();
+    printf(" The ASCII code of %c is %d",i,i);
+}
